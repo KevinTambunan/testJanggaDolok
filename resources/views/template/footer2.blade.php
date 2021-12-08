9ec2c73b31943635813e7f57505ed6ab1720d1db
KevinTambunan <tambunankevin8@gmail.com>
2021-12-08 18:46:13 +0700

    peta

--- resources/views/template/footer2.blade.php
+++ resources/views/template/footer2.blade.php
@@ -20,7 +20,7 @@
                                 </div>
                                 <div class="row">
                                     <div class="col-md-12">
-                                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7973.024155525846!2d98.90705794624067!3d2.332697493166285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x302e19682effabef%3A0x59f6719f88c544ef!2sHutana%20Godang%2C%20Muara%2C%20Kabupaten%20Tapanuli%20Utara%2C%20Sumatera%20Utara!5e0!3m2!1sid!2sid!4v1638962268170!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63776.028915962306!2d98.90948661865835!3d2.506277030163091!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3031e3f4e2a546c3%3A0xef4d8c88f8090eb0!2sOnan%20Runggu%2C%20Kabupaten%20Samosir%2C%20Sumatera%20Utara!5e0!3m2!1sid!2sid!4v1638963915076!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
                                     </div>
                                 </div>
                             </div>
