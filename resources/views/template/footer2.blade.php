c25634d4604bda09e914426739cf88db8cd9763b
KevinTambunan <tambunankevin8@gmail.com>
2021-12-08 12:04:07 +0700

    tambah peta

--- resources/views/template/footer2.blade.php
+++ resources/views/template/footer2.blade.php
@@ -20,10 +20,7 @@
                                 </div>
                                 <div class="row">
                                     <div class="col-md-12">
-                                        <iframe
-                                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24451.65170819838!2d99.05137381728848!3d2.5780028797802284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3031fa706e04b3fd%3A0xac53ac2caf514e77!2sJangga%20Dolok%2C%20Lumban%20Julu%2C%20Kabupaten%20Toba%20Samosir%2C%20Sumatera%20Utara!5e1!3m2!1sid!2sid!4v1613055598472!5m2!1sid!2sid"
-                                            class="w-100" height="300" frameborder="0" style="border:0;"
-                                            allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15945.844117181921!2d99.01992917625886!3d2.350639472589742!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x302e04a73872f6c5%3A0x6da936cc86a7ab79!2sTara%20Bunga%2C%20Tampahan%2C%20Toba%2C%20Sumatera%20Utara!5e0!3m2!1sid!2sid!4v1638939694620!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 
                                     </div>
                                 </div>
