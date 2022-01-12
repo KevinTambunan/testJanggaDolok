b7042ffe19b1b4d6ffdae17c584e0094903fe064
KevinTambunan <tambunankevin8@gmail.com>
2022-01-12 23:47:24 +0700

    ok

--- resources/views/template/footer2.blade.php
+++ resources/views/template/footer2.blade.php
@@ -21,9 +21,10 @@
                                 <div class="row">
                                     <div class="col-md-12">
                                         <iframe
-                                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31891.688222313187!2d99.028684!3d2.35064!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x302e04a73872f6c5%3A0x6da936cc86a7ab79!2sTara%20Bunga%2C%20Tampahan%2C%20Toba%2C%20Sumatera%20Utara!5e0!3m2!1sid!2sid!4v1642003317415!5m2!1sid!2sid"
+                                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d63784.910415811806!2d99.001376!3d2.316829!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x302e1b2618ee6569%3A0x36e2c26fb20124ca!2sMeat%2C%20Tampahan%2C%20Toba%2C%20Sumatera%20Utara!5e0!3m2!1sid!2sid!4v1642005982554!5m2!1sid!2sid"
                                             class="w-100" height="300" frameborder="0" style="border:0;"
                                             allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
 
 
                                     </div>
